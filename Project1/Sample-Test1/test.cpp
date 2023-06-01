b25cf7a2a5b6749f644887dd3aa8f5d036cd7bbf
chanwoo <pakchanwoo@gmail.com>
2023-06-01 14:45:54 +0900

    commit

--- Project1/Sample-Test1/test.cpp
+++ Project1/Sample-Test1/test.cpp
@@ -1,7 +1,22 @@
 #include "pch.h"
 #include "..\Project1\bank.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class AccountFixture : public testing::Test
+{
+public:
+	Account account{ 10000 };
+};
+
+TEST_F(AccountFixture, CreateAccountInit10000won) {
+	EXPECT_EQ(10000, account.getBalance());
+}
+
+TEST_F(AccountFixture, Deposit) {
+	account.deposit(500);
+	EXPECT_EQ(10500, account.getBalance());
+}
+
+TEST_F(AccountFixture, Withdraw) {
+	account.Withdraw(600);
+	EXPECT_EQ(9400, account.getBalance());
 }
